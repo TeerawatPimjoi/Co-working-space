a6e5a5d99d0b34ebea25ced41343007e084f8157
Teerawat Pimjoi <tpimjoi@gmail.com>
2022-05-01 21:55:20 +0700

    add authentication

--- routes/coworkingspaces.js
+++ routes/coworkingspaces.js
@@ -1,8 +1,22 @@
-const express = require('express');
-const {getcoworkingspaces,getcoworkingspace,createcoworkingspace,updatecoworkingspace,deletecoworkingspace} = require('../controllers/coworkingspaces');
+const express = require("express");
+const {
+  getcoworkingspaces,
+  getcoworkingspace,
+  createcoworkingspace,
+  updatecoworkingspace,
+  deletecoworkingspace
+} = require("../controllers/coworkingspaces");
 const router = express.Router();
+const { protect, authorize } = require("../middleware/auth");
 
-router.route('/').get(getcoworkingspaces).post(createcoworkingspace);
-router.route('/:id').get(getcoworkingspace).put(updatecoworkingspace).delete(deletecoworkingspace);
+router
+  .route("/")
+  .get(getcoworkingspaces)
+  .post(protect, authorize("admin"), createcoworkingspace);
+router
+  .route("/:id")
+  .get(getcoworkingspace)
+  .put(protect, authorize("admin"), updatecoworkingspace)
+  .delete(protect, authorize("admin"), deletecoworkingspace);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
 
 //load env vars
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
 //connect to database
 connectDB();
 
-const app =express();
+const app = express();
 //body parser
 app.use(express.json());
 
-const coworkingspaces = require('./routes/coworkingspaces');
-app.use('/api/v1/coworkingspaces',coworkingspaces)
+//Cookie parser
+app.use(cookieParser());
+
+const coworkingspaces = require("./routes/coworkingspaces");
+const auth = require("./routes/auth");
+app.use("/api/v1/coworkingspaces", coworkingspaces);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT,console.log('Server running in' , process.env.NODE_ENV, 'mode on port',PORT));
+const server = app.listen(
+  PORT,
+  console.log("Server running in", process.env.NODE_ENV, "mode on port", PORT)
+);
 //Handle unhandled promise rejections
-process.on('unhandledRejection',(err,promise)=>{
+process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`);
   //Close server & exit process
-  server.close(()=>process.exit(1)); 
+  server.close(() => process.exit(1));
 });
